9abe0824ac1e43dd9e916b0a964557eb2064639e
NitroTech 4K <crtraer@gmail.com>
2022-10-11 08:56:25 -0300

    Made an if statement

--- Program.cs
+++ Program.cs
@@ -10,6 +10,27 @@ namespace SelectionStatements1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello world of selction statements!");
+            Console.WriteLine("===================================");
+            Console.WriteLine();
+
+            string gamertag;
+            int score;
+
+
+            score = 0;
+            gamertag = "NitroTech 4K";
+
+            if (gamertag == "NitroTech 4K")
+            {
+                score = 1000;
+            }
+
+
+            Console.WriteLine("Score: " + score);
+            
+
+            Console.ReadKey(true);
         }
     }
 }
